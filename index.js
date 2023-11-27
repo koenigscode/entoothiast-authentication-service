97800f763ded2482d44115a8f372dc6a112b13d7
Michael Koenig <koenigmichael2002@gmail.com>
2023-11-27 20:52:31 +0100

    Return status codes for login and register #2 #3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "very secret secret"
 
 const client = mqtt.connect(process.env.BROKER_URL)
 /** @type {MqttRequest}*/
-const mqttReq = new MqttRequest.default(client);
+export const mqttReq = new MqttRequest.default(client);
 
 console.log(`Broker URL: ${process.env.BROKER_URL}`)
 
@@ -19,46 +19,34 @@ console.log(`Broker URL: ${process.env.BROKER_URL}`)
 mqttReq.response("v1/users/register", (payload) => {
     const { username, password, name, role } = JSON.parse(payload)
     const passwordHash = bcrypt.hashSync(password, PW_SALT_ROUNDS)
+
     try {
         db.querySync("insert into public.user (username, pw_hash, name, role) values ($1, $2, $3, $4)", [username, passwordHash, name, role])
-        return JSON.stringify({ error: true, message: "Registered user" })
+        return JSON.stringify({ httpStatus: 201, message: `Registered user ${username}` })
     } catch (e) {
         console.log(e)
-        return JSON.stringify({ error: true, message: "Username already exists" })
+        return JSON.stringify({ httpStatus: 400, message: `Username ${username} already exists` })
     }
 });
 
-mqttReq.response("v1/users/authentication", (payload) => {
+mqttReq.response("v1/users/login", (payload) => {
     const { username, password } = JSON.parse(payload)
     const users = db.querySync("select username, name, pw_hash, role from public.user where username = $1", [username])
+
     if (!users || users.length == 0)
-        return JSON.stringify({ error: true, message: "User not found" })
+        return JSON.stringify({ httpStatus: 404, message: "User not found" })
 
     if (bcrypt.compareSync(password, users[0].pw_hash)) {
         const user = users[0]
         const token = jwt.sign({ username: user.username, role: user.role, name: user.name }, JWT_SECRET)
-        return JSON.stringify({ error: false, message: "Authentication successful", token })
+        return JSON.stringify({ httpStatus: 200, message: "Authentication successful", token })
     }
 
-    return JSON.stringify({ error: true, message: "Authentication failed" })
+    return JSON.stringify({ httpStatus: 400, message: "Wrong password" })
 })
 
 
 
 client.on("connect", async () => {
-    mqttReq.request("v1/users/register",
-        (payload) => {
-            console.log("Get response:" + payload.toString());
-        },
-        JSON.stringify({ username: "test", password: "test", name: "Name", role: "patient" })
-    )
-    mqttReq.request("v1/users/authentication",
-        (payload) => {
-            console.log("Get response:" + payload.toString());
-            console.log(jwt.decode(JSON.parse(payload).token))
-        },
-        JSON.stringify({ username: "test", password: "test" })
-    )
-
     console.log("authentication-service connected to broker")
 });
